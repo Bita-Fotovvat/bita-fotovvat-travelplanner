2c1b257501862924bf843ec03bab6f952e49632f
Bita Fotovvat <bita.ftvt@gmail.com>
2024-02-17 12:34:08 -0500

    initial header structure added

---
+++ travelplanner/src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import "./Header.scss";
+
+export default function Header(){
+
+    return(
+        <header className="header">
+            <nav className="header__nav">
+                <ul className="header__navli">
+                    <li className="header__navli--item">Home</li>
+                    <li className="header__navli--item">Plan My Trip</li>
+                    <li className="header__navli--item">My Profile</li>
+                    <li className="header__navli--item">Log In</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
