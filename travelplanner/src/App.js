2e1a114f6dc8d7dd18400ec856de5ae4517a8737
Bita Fotovvat <bita.ftvt@gmail.com>
2024-02-17 12:38:41 -0500

    first basic version of header component added

--- travelplanner/src/App.js
+++ travelplanner/src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
 import './App.scss';
+import Header from './components/Header/Header';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
+
   return (
-    <>
-    </>
+    <BrowserRouter>
+    <Header/>
+    <Routes></Routes>
+    </BrowserRouter>
   );
 }
