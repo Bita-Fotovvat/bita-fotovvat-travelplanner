0a7133efc8bb735b606f6e6dcb8f20e6f3fa04a6
Bita Fotovvat <bita.ftvt@gmail.com>
2024-02-17 19:27:31 -0500

    added & completed the list component

---
+++ travelplanner/src/components/PlaceDetails/PlaceDetails.js
@@ -0,0 +1,8 @@
+import "./PlaceDetails.scss";
+
+export default function PlaceDetails({place}){
+
+    return(
+        <h1>{place.name}</h1>
+    );
+}
--- travelplanner/src/pages/HomePage/HomePage.js
+++ travelplanner/src/pages/HomePage/HomePage.js
@@ -1,5 +1,6 @@
 import "./HomePage.scss";
 import Map from "../../components/Map/Map";
+import List from "../../components/List/List";
 
 export default function HomePage(){
     return(
